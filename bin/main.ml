25a5f646cec1c51c662dd2677acc463ede67a8af
Jay Lee <jaeho.lee@snu.ac.kr>
2022-07-27 03:12:44 +0900

    :sparkles: Add command line interface
    and read from a file.
    Also, in order to keep the code clean with these added functionality,
    some code was refactored to use the List monad (actually with as an
    applicative).
    Further refactorization can be done.

--- bin/main.ml
+++ bin/main.ml
@@ -1,33 +1,48 @@
 open Analyzer
 open Shape_analyzer
 
-let l_program = Util.get_program stdin
+let usage_msg = "[-shape]"
+let shape_analysis = ref false
+let input_files = ref []
+let anon_fun filename = input_files := filename :: !input_files
+let speclist = [ ("-shape", Arg.Set shape_analysis, "Shape analysis") ]
 
-(* samples is a list of input-output pairs *)
-let version, samples, root_expr = l_program
-let () = Util.check_version version root_expr
+let _ =
+  Arg.parse speclist anon_fun usage_msg;
 
-let converted_samples =
-  List.map (fun (i, o) -> (L.hvalue_of_vvalue i, L.hvalue_of_vvalue o)) samples
+  let open Monads.List in
+  let+ filename = !input_files in
 
-(* Process version *)
-(* let _ = print_string "Interpreter version: L" *)
-let _ = print_string "Type checker version: L"
-let _ = print_int version
-let _ = print_newline ()
+  let l_program = filename |> In_channel.open_text |> Util.get_program in
+  (* samples is a list of input-output pairs *)
+  let version, samples, root_expr = l_program in
+  let () = Util.check_version version root_expr in
 
-let out_types =
-  let type_of_hvalue hvalue =
-    match type_of_hvalue hvalue with
-    | Some t -> t
-    | None -> failwith "Hole should not exist in output"
+  let converted_samples =
+    let+ i, o = samples in
+    L.(hvalue_of_vvalue i, hvalue_of_vvalue o)
   in
-  List.map
-    (fun (i, t) -> type_check (L.Let ("x", i, root_expr)) t)
-    (List.map
-       (fun p -> (L.expr_of_hvalue @@ fst @@ p, type_of_hvalue @@ snd @@ p))
-       converted_samples)
 
-let () =
-  if List.flatten out_types = [] then print_endline "Unsatisfiable!"
-  else List.iter print_type_list out_types
+  if !shape_analysis then (
+    (* Process version *)
+    (* let _ = print_string "Interpreter version: L" *)
+    Printf.printf "Shape analyzer version: L%d\n" version;
+    let out_types =
+      let type_of_hvalue hvalue =
+        match type_of_hvalue hvalue with
+        | Some t -> t
+        | None -> failwith "Hole should not exist in output"
+      in
+      List.map
+        (fun (i, t) -> type_check (L.Let ("x", i, root_expr)) t)
+        (List.map
+           (fun p -> (L.expr_of_hvalue @@ fst @@ p, type_of_hvalue @@ snd @@ p))
+           converted_samples)
+    in
+
+    let () =
+      if List.flatten out_types = [] then print_endline "Unsatisfiable!"
+      else List.iter print_type_list out_types
+    in
+    ())
+  else ()
--- lib/monads.ml
+++ lib/monads.ml
@@ -5,21 +5,64 @@ module type Monad = sig
   val bind : 'a t -> ('a -> 'b t) -> 'b t
 end
 
-module Syntax (M : Monad) = struct
+module MonadSyntax (M : Monad) = struct
   let ( >>= ) = M.bind
   let ( let* ) = M.bind
 end
 
-module OptionBase : Monad with type 'a t = 'a option = struct
+module type Applicative = sig
+  type 'a t
+
+  val map : ('a -> 'b) -> 'a t -> 'b t
+  val product : 'a t -> 'b t -> ('a * 'b) t
+end
+
+module ApplicativeSyntax (A : Applicative) = struct
+  let ( >>| ) o f = A.map f o
+  let ( let+ ) = ( >>| )
+  let ( and+ ) = A.product
+end
+
+module OptionBaseMonad : Monad with type 'a t = 'a option = struct
   type 'a t = 'a option
 
   let return x = Some x
-  let bind o f = match o with Some x -> f x | None -> None
+  let bind o f = match o with None -> None | Some x -> f x
 end
 
-module OptionSyntax = Syntax (OptionBase)
+module OptionBaseApplicative : Applicative with type 'a t = 'a option = struct
+  type 'a t = 'a option
+
+  let map f = function None -> None | Some x -> Some (f x)
+
+  let product o1 o2 =
+    match (o1, o2) with Some x, Some y -> Some (x, y) | _ -> None
+end
 
 module Option = struct
-  include OptionBase
-  include OptionSyntax
+  include OptionBaseMonad
+  include MonadSyntax (OptionBaseMonad)
+  include OptionBaseApplicative
+  include ApplicativeSyntax (OptionBaseApplicative)
+end
+
+module ListBaseMonad : Monad with type 'a t = 'a list = struct
+  type 'a t = 'a list
+
+  let return x = [ x ]
+  let bind o f = List.concat_map f o
+end
+
+module ListBaseApplicative : Applicative with type 'a t = 'a list = struct
+  type 'a t = 'a list
+
+  let map = List.map
+  let product = List.combine
+end
+
+module List = struct
+  include ListBaseMonad
+  include MonadSyntax (ListBaseMonad)
+  include ListBaseApplicative
+  include ApplicativeSyntax (ListBaseApplicative)
 end
