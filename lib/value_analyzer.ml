5a5bad1e52497d2077087e70a703ae3c5eab5127
Jay Lee <jaeho.lee@snu.ac.kr>
2022-08-04 00:02:51 +0900

    :recycle: Refactor value_analyzer

--- lib/value_analyzer.ml
+++ lib/value_analyzer.ml
@@ -1,6 +1,7 @@
 open L
 
 (* Types and exceptions *)
+
 module HoleCoeffs = struct
   open Monads.List
 
@@ -10,7 +11,8 @@ module HoleCoeffs = struct
     List.find_opt (( <> ) 0) hole_coeffs |> Option.is_none
 
   let length = List.length
-  let make = return
+  let make_num = return
+  let add_hole ?coeff:(k = 1) hole_coeffs = hole_coeffs @ make_num k
 
   let rec ( +! ) h1 h2 =
     match (h1, h2) with
@@ -24,20 +26,21 @@ end
 
 (** Eventually, hvalue type must be generalized and use the following as well *)
 type hvalue' =
-  | HHole' of int
   | HNum' of HoleCoeffs.t
   | HPair' of hvalue' * hvalue'
 
-let rec hvalue'_of_hvalue = function
-  | HHole -> HHole' 1
-  | HNum n -> HNum' (HoleCoeffs.make n)
+let rec hvalue'_of_hvalue =
+  let open HoleCoeffs in
+  function
+  | HHole -> HNum' (add_hole (make_num 0))
+  | HNum n -> HNum' (make_num n)
   | HPair (h1, h2) -> HPair' (hvalue'_of_hvalue h1, hvalue'_of_hvalue h2)
 
 let rec hvalue_of_hvalue' = function
-  | HHole' 1 -> HHole
   | HNum' [ n ] -> HNum n
+  | HNum' [ 0; 1 ] -> HHole
+  | HNum' _ -> failwith "[WIP] Arbitrary HNum' cannot be converted."
   | HPair' (h1, h2) -> HPair (hvalue_of_hvalue' h1, hvalue_of_hvalue' h2)
-  | _ -> failwith "[WIP] Conversion failure!"
 
 type id = string
 type env = id -> hvalue'
@@ -56,6 +59,14 @@ type cond_eqn = HoleCoeffs.t * comp_op
 exception TypeError of string
 exception RunError of string
 
+let raiseTypeError expected op =
+  let expected, current =
+    let n, p = ("number", "pair") in
+    match expected with `Num -> (n, p) | `Pair -> (p, n)
+  in
+  let msg = Printf.sprintf "%s: %s expected, not a %s" op expected current in
+  raise (TypeError msg)
+
 let empty_env (_ : id) : hvalue' = raise (RunError "undefined variable")
 
 (** Environment augmentation. Use @: to bind (x, v) to f *)
@@ -64,36 +75,31 @@ let ( @: ) (x, v) e y = if y = x then v else e y
 let rec eval env expr =
   let open HoleCoeffs in
   match expr with
-  | Hole -> HHole' 1
-  | Num n -> HNum' (make n)
+  | Hole -> HNum' (add_hole (make_num 0))
+  | Num n -> HNum' (make_num n)
   | Pair (e1, e2) ->
       let v1 = eval env e1 in
       let v2 = eval env e2 in
       HPair' (v1, v2)
   | Fst e -> (
-      let pair = eval env e in
-      match pair with
+      match eval env e with
       | HPair' (fst, _) -> fst
-      | _ -> raise (TypeError "FIRST: not a pair"))
+      | HNum' _ -> raiseTypeError `Pair "FIRST")
   | Snd e -> (
-      let pair = eval env e in
-      match pair with
+      match eval env e with
       | HPair' (_, snd) -> snd
-      | _ -> raise (TypeError "SECOND: not a pair"))
+      | HNum' _ -> raiseTypeError `Pair "SECOND")
   | Add (e1, e2) -> (
-      let lhs = eval env e1 in
-      match lhs with
+      match eval env e1 with
       | HNum' lhs_n -> (
-          let rhs = eval env e2 in
-          match rhs with
+          match eval env e2 with
           | HNum' rhs_n -> HNum' (lhs_n +! rhs_n)
-          | _ -> raise (TypeError "ADD: lhs not a number"))
-      | _ -> raise (TypeError "ADD: rhs not a number"))
+          | HPair' _ -> raiseTypeError `Num "ADD")
+      | HPair' _ -> raiseTypeError `Num "ADD")
   | Neg e -> (
-      let num = eval env e in
-      match num with
+      match eval env e with
       | HNum' n -> HNum' ~-!n
-      | _ -> raise (TypeError "NEGATE: not a number"))
+      | HPair' _ -> raiseTypeError `Num "NEGATE")
   | Case (x, y, z, e1, e2) -> (
       let v = eval env x in
       match v with
@@ -105,7 +111,7 @@ let rec eval env expr =
       let v = eval env pred in
       match v with
       | HNum' n -> eval env (if is_zero n then false_e else true_e)
-      | _ -> raise (TypeError "IF: pred not a number"))
+      | HPair' _ -> raiseTypeError `Num "IF")
   | Let (x, exp, body) ->
       let v = eval env exp in
       eval ((x, v) @: env) body
